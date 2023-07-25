7ec59f240393a418ad07300349392bd518930bdc
Ezequiel-Front-End <ezequielfernandes912@gmail.com>
2023-07-25 17:08:46 -0300

    newCode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav/nav.component';
+import { HomeComponent } from './home/home/home.component';
+import { PapelariaComponent } from './papelaria/papelaria/papelaria.component';
+import { ContabilidadeComponent } from './contabilidade/contabilidade/contabilidade.component';
+import { AdministracaoComponent } from './administracao/administracao/administracao.component';
+import { DepartamentoPessoalComponent } from './departamento-pessoal/departamento-pessoal/departamento-pessoal.component';
+import { DepartamentoFiscalComponent } from './departamento-fiscall/departamento-fiscal/departamento-fiscal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    PapelariaComponent,
+    ContabilidadeComponent,
+    AdministracaoComponent,
+    DepartamentoPessoalComponent,
+    DepartamentoFiscalComponent
   ],
   imports: [
     BrowserModule,
